1e8462aaf106045c3012a62b3466ccfc29e9f58c
ahmed <ahmedsamirio95@gmail.com>
2020-07-10 18:05:30 +0200

    completely refactored make_row into make_ad_dict

--- functions.py
+++ functions.py
@@ -8,83 +8,71 @@ from config import *
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
 logging.disable(logging.DEBUG)
 
+def get_full_ad_info(link_soup):
+    """ Returns the full ad infomation in a readable format """
+    full_raw_info = link_soup.select('strong a')  # full ad information in soupified html format
+    full_processed_info = []
+    for info in raw_info:
+        processed_info = re.sub('\t|\n', '', info.get_text())
+        full_processed_info.append(processed_info)
+    return full_processed_info
 
-def make_row(link_soup, link):
+def get_full_ad_info_ids(link_soup):
+    """ Returns the ad information identifiers (headers) """
+    raw_ids = link_soup.select('th')  # identifiers in soupified html format
+    processed_ids = [id.get_text() for id in raw_ids]
+    return processed_ids
+
+def get_info_from_id(id, full_info, full_info_ids):
+    """ Return a single ad info by specifing it's id counterpart """
+    if id in info_ids:
+        info_idx = full_info_ids.index(id)  # get the id index to return its info counterpart
+        return full_info[info_idx]
+
+def get_car_features(full_info, full_info_ids):
+    """ Returns a list containig all car features mentioned in the ad """
+    car_features = full_info[7:-3]  # the slice of the full info containing the ads
+
+    # since all car features are accounted for with only one id, we need to remove
+    # these features and their counterpart id in order to be able to extract 
+    # the remaining ad info by using the index counterpart way used in get_info_from_id
+    for feature in car_features:
+        full_info.remove(feature)
+    full_info_ids.remove('إضافات')
+    return car_features
+
+
+def make_ad_dict(link_soup, link):
+    """ Returns a dictionary with all the relevant ad info """
     
-    # Create row dict containing the column names
-    row = dict.fromkeys(['Brand', 'Model', 'Governerate', 'City', 'Date', 'Year', 'Kilometers', 'Pay_type',
+    ad_dict = dict.fromkeys(['Brand', 'Model', 'Governerate', 'City', 'Date', 'Year', 'Kilometers', 'Pay_type',
                          'Ad_type','Transmission', 'CC', 'Chasis', 'Features', 'Color', 'Price', 'URL'])
-    
-    # car price
-    row['Price'] = get_price(link_soup)
-    
-    # ad link
-    row['URL'] = link['href']
 
-    # ad date
-    row['Date'] = get_date(link_soup)
+    ad_dict['Date'] = get_date(link_soup)
+    ad_dict['Price'] = get_price(link_soup)
+    ad_dict['Brand'] = get_brand(link_soup, ad_dict['City'])
+    ad_dict['City'], ad_dict['Governerate'] = get_ad_location(link_soup)
+    ad_dict['URL'] = link['href']
 
-    # ad location
-    row['City'], row['Governerate'] = get_ad_location(link_soup)
+    ad_info = get_ad_info(link_soup)
+    ad_info_ids = get_ad_info_ids(link_soup)
 
-    # car brand
-    row['Brand'] = get_brand(link_soup, row['City'])
+    ad_dict['CC'] = get_info_from_id('المحرك (سي سي)', ad_info, ad_info_ids)
+    ad_dict['Year'] = get_info_from_id('السنة', ad_info, ad_info_ids)
+    ad_dict['Model'] = get_info_from_id('موديل', ad_info, ad_info_ids)
+    ad_dict['State'] = get_info_from_id('الحالة', ad_info, ad_info_ids)
+    ad_dict['Pay_type'] = get_info_from_id('طريقة الدفع', ad_info, ad_info_ids)
+    ad_dict['Kilometers'] = get_info_from_id('كيلومترات', ad_info, ad_info_ids)
+    ad_dict['Transmission'] = get_info_from_id('ناقل الحركة', ad_info, ad_info_ids)
 
-    # scrape the full info the ad 
-    full_info = []
-    for info in link_soup.select('strong a'):
-        # collect all ad info from html
-        full_info.append(re.sub('\t|\n', '', info.get_text()))
-        
-    # collect the headers for the ad info in order to sort them into their places
-    ad_info_id = [x.get_text() for x in link_soup.select('th')]
+    if 'إضافات' in ad_info_ids:
+        ad_dict['Features'] = get_car_features(full_info, full_info_ids)
+
+    ad_dict['Color'] = get_info_from_id('اللون', ad_info, ad_info_ids)
+    ad_dict['Chasis'] = get_info_from_id('نوع الهيكل', ad_info, ad_info_ids)
+    ad_dict['Ad_type'] = get_info_from_id('نوع الإعلان', ad_info, ad_info_ids)
     
-    for info_id, info in zip(ad_info_id[:7], full_info[:7]):
-        if info_id == 'موديل':
-            row['Model'] = info
-        elif info_id == 'ناقل الحركة':
-            row['Transmission'] = info
-        elif info_id == 'السنة':
-            row['Year'] = info
-        elif info_id == 'كيلومترات':
-            row['Kilometers'] = info
-        elif info_id == 'طريقة الدفع':
-            row['Pay_type'] = info
-        elif info_id == 'الحالة':
-            row['State'] = info
-        elif info_id == 'المحرك (سي سي)':
-            row['CC'] = info
-            
-    # make a separate list for car features
-    car_features = []
-    
-    # make a copy for ad info so as to remove all features except one, to add other features that go after
-    full_info_c = full_info.copy()
-
-    for i, info in enumerate(full_info[7:]):
-        if info in ['EBD', 'إطارات خاصة','باور ستيرنج','بلوتوث', 'تكييف', 'تنبيه / نظام مضاد للسرقة', 'حامل السقف',
-                     'حساسات ركن', 'راديو اف ام', 'زجاج كهربائي', 'زر تشغيل / إيقاف المحرك', 'سنتر لوك',
-                     'شاحن يو اس بي', 'شاشة تعمل باللمس', 'عجلات للطرق الوعرة', 'فتحة سقف', 'فوانيس شبورة',
-                     'كاميرا خلفية', 'مثبت سرعة', 'مدخل aux اوديو', 'مرايا كهربائية', 'مقاعد جلد', 'مقاعد كهربائية',
-                     'نظام فرامل ABS', 'نظام ملاحة', 'وسائد هوائية']:
-            car_features.append(info)
-            if (i + 3) != len(full_info[7:]):
-                full_info_c.remove(info)
-                
-    # removes the features id since all features are removed            
-    if 'إضافات' in ad_info_id:
-        ad_info_id.remove('إضافات')
-    
-    row['Features'] = car_features
-    for info_id, info in zip(ad_info_id[7:], full_info_c[7:]):
-        if info_id == 'اللون':
-            row['Color'] = info
-        elif info_id == 'نوع الهيكل':
-            row['Chasis'] = info
-        elif info_id == 'نوع الإعلان':
-            row['Ad_type'] = info
-            
-    return row
+    return ad_dict
 
 def get_date(link_soup):
     try:
@@ -134,7 +122,7 @@ def get_res(url, MAX_RETRIES, headers):
 
 
 def scrape_ad(link, headers, sem, sleep=False):
-    global all_rows
+    global all_ad_dicts
     
     sem.acquire(blocking=False)
 
@@ -144,8 +132,8 @@ def scrape_ad(link, headers, sem, sleep=False):
 
     link_res = get_res(link['href'], max_retries, headers)
     link_soup = bs4.BeautifulSoup(link_res.text, features="lxml")
-    row = make_row(link_soup, link)
-    all_rows.append(row)
+    ad_dict = make_ad_dict(link_soup, link)
+    all_ad_dicts.append(ad_dict)
     
     sem.release()
     
